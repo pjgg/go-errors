c17c2dc2f296aaf48c18e51916430089a74ca345
ivan-garcia-santmaria <ivan.garcias@masmovil.com>
2019-02-27 16:49:16 +0100

    export raven client and rename constructor for GetInstance

--- errors/handler_test.go
+++ errors/handler_test.go
@@ -79,7 +79,7 @@ type ErrorHandlerTestSuite struct {
 }
 
 func (suite *ErrorHandlerTestSuite) SetupTest() {
-	suite.errHandler = NewErrorHandler("QA", "https://1eecf0d5795b4f9bbc5f4210c1513f4c:35ae8d2ab04144d8a01127612ed810d9@sentry.MM.com/132", "mock", true)
+	suite.errHandler = GetInstance("QA", "https://1eecf0d5795b4f9bbc5f4210c1513f4c:35ae8d2ab04144d8a01127612ed810d9@sentry.MM.com/132", "mock", true)
 }
 
 func TestErrorHandlerTestSuite(t *testing.T) {
